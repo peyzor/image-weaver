7f905bc299674ef87489caf3911ac9d17c8c8038
shaghayegh <valadkhani.sh@gmail.com>
2023-12-14 11:13:24 +0330

    add celery config

---
+++ image_weaver/celery.py
@@ -0,0 +1,28 @@
+import os
+
+from celery import Celery, signals
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_weaver.settings')
+
+app = Celery('image_weaver')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+
+
+@signals.setup_logging.connect
+def config_loggers(*args, **kwargs):
+    from logging.config import dictConfig
+    from django.conf import settings
+    dictConfig(settings.LOGGING)
