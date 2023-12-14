2c24ca28ff335a9c968b61ab41bbaa834fecd222
Peyman <hassani.peyman627@gmail.com>
2023-12-14 09:51:28 +0330

    Add description model

--- descriptions/models.py
+++ descriptions/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from common.base_models import BaseModel
+
+
+class Description(BaseModel):
+    description = models.TextField(verbose_name=_('description'))
