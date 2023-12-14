c7bcac54f6a86fd454c55d12df67eb16ae00b2ed
Peyman <hassani.peyman627@gmail.com>
2023-12-14 09:56:59 +0330

    Add generated image model

--- images/models.py
+++ images/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from common.base_models import BaseModel
+
+
+class GeneratedImage(BaseModel):
+    description = models.ForeignKey(to='descriptions.Description', verbose_name=_('description'),
+                                    on_delete=models.PROTECT)
+    image = models.ImageField(verbose_name=_('image'), upload_to='generated_images/%Y/%m/')
